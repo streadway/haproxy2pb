ae948e6107c5ab66e9406ce659c1ddb19d307a9a
Sean Treadway <sean@soundcloud.com>
2012-07-11 23:04:51 +0200

    go fmt

--- haproxy2pb/main.go
+++ haproxy2pb/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"os"
 	"bufio"
-	"haproxy"
+	proto "code.google.com/p/goprotobuf/proto"
 	"fmt"
-  proto "code.google.com/p/goprotobuf/proto"
+	"haproxy"
+	"os"
 )
 
-func readLine(r *bufio.Reader) ([]byte, error) { 
-  line, isPrefix, err := r.ReadLine() 
-  if !isPrefix { 
-    return line, err 
-  } 
-  buf := append([]byte(nil), line...) 
-  for isPrefix && err == nil { 
-    line, isPrefix, err = r.ReadLine() 
-    buf = append(buf, line...) 
-  } 
-  return buf, err 
-} 
+func readLine(r *bufio.Reader) ([]byte, error) {
+	line, isPrefix, err := r.ReadLine()
+	if !isPrefix {
+		return line, err
+	}
+	buf := append([]byte(nil), line...)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		buf = append(buf, line...)
+	}
+	return buf, err
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -36,7 +36,7 @@ func main() {
 			if err != nil {
 				fmt.Println("err", err)
 			}
-			fmt.Println(len(buf), len(line), len(line) - len(buf))
+			fmt.Println(len(buf), len(line), len(line)-len(buf))
 			//fmt.Println(pb.String())
 			//fmt.Println("ok", *pb.Timestamp)
 		} else {
--- scan.go
+++ scan.go
@@ -2,11 +2,11 @@ package haproxy
 
 import (
 	"fmt"
-	"regexp"
 	"net"
-	"time"
-	"strings"
+	"regexp"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // Oct 28 14:30:05 ams-mid006.int.s-cloud.net haproxy[10227]: 10.20.3.41:45572 [28/Oct/2011:14:30:05.615] soundcloud soundcloud/ams-app053-8003 0/0/0/18/18 302 677 - - ---- 157/126/126/0/0 0/0 {med} {7} "GET /stream/pNAFM5DjlcZj?url=http%3A//api.soundcloud.com/users/4641029&referer=http%3A//static.ak.facebook.com/common/referer_frame.php&auto_play=true&height=209&color=3b5998&width=398&show_artwork=false&flash_version=MAC%2011%2C0%2C1%2C152&consumer_key=sc_player HTTP/1.0"
@@ -21,9 +21,9 @@ var Reason_short = map[string]string{
 	"C": "CLIENT_ABORT",
 	"S": "SERVER_ABORT",
 	"P": "PROXY_ABORT",
-	"R": "RESOURCE_LIMIT", 
-	"I": "INTERNAL_ERROR", 
-	"c": "CLIENT_TIMEOUT", 
+	"R": "RESOURCE_LIMIT",
+	"I": "INTERNAL_ERROR",
+	"c": "CLIENT_TIMEOUT",
 	"s": "SERVER_TIMEOUT",
 }
 var ProxyState_short = map[string]string{
@@ -45,7 +45,7 @@ var Cookie_short = map[string]string{
 	"O": "OLD",
 }
 
-var CookieTransform_short =map[string]string{
+var CookieTransform_short = map[string]string{
 	"N": "NONE",
 	"I": "INSERTED",
 	"U": "UPDATED",
@@ -54,7 +54,7 @@ var CookieTransform_short =map[string]string{
 	"D": "DELETED",
 }
 
-func _ip(str string) ([]byte) {
+func _ip(str string) []byte {
 	if i := net.ParseIP(str); i != nil {
 		return i
 	}
@@ -81,15 +81,15 @@ func scanTime(str string, req *Request) {
 	}
 }
 
-func _time(str string) (*float32) {
+func _time(str string) *float32 {
 	if t, err := time.Parse(EventTimeLayout, str); err == nil {
-		var seconds = float32(t.Unix()) + float32(t.UnixNano()) / 1e6
+		var seconds = float32(t.Unix()) + float32(t.UnixNano())/1e6
 		return &seconds
 	}
 	return nil
 }
 
-func _int32(str string) (*int32) {
+func _int32(str string) *int32 {
 	if i, err := strconv.ParseInt(str, 10, 32); err == nil {
 		i32 := int32(i)
 		return &i32
@@ -97,7 +97,7 @@ func _int32(str string) (*int32) {
 	return nil
 }
 
-func _uint32(str string) (*uint32) {
+func _uint32(str string) *uint32 {
 	if i, err := strconv.ParseInt(str, 10, 32); err == nil {
 		ui32 := uint32(i)
 		return &ui32
@@ -107,15 +107,15 @@ func _uint32(str string) (*uint32) {
 
 func scanTimings(str string, req *Request) {
 	if times := strings.Split(str, "/"); len(times) >= 5 {
-		req.TimeQueue                  = count(times[0])
-		req.TimeWait                   = count(times[1])
-		req.TimeConnect                = count(times[2])
-		req.TimeRespond                = count(times[3])
-		req.TimeTotal                  = count(times[4])
+		req.TimeQueue = count(times[0])
+		req.TimeWait = count(times[1])
+		req.TimeConnect = count(times[2])
+		req.TimeRespond = count(times[3])
+		req.TimeTotal = count(times[4])
 	}
 }
 
-func count(str string) (*int32) {
+func count(str string) *int32 {
 	if i, err := strconv.ParseInt(str, 10, 32); err == nil {
 		if i >= 0 {
 			i32 := int32(i)
@@ -138,7 +138,7 @@ func scanTermination(str string, req *Request) {
 	}
 }
 
-func termination(short string, shortMap map[string]string, enumMap map[string]int32) (*int32) {
+func termination(short string, shortMap map[string]string, enumMap map[string]int32) *int32 {
 	enum := shortMap[short]
 	if enum != "" {
 		tag := enumMap[enum]
@@ -151,22 +151,22 @@ func termination(short string, shortMap map[string]string, enumMap map[string]in
 
 func scanConnections(str string, req *Request) {
 	if counts := strings.Split(str, "/"); len(counts) >= 5 {
-		req.ActiveConnections          = count(counts[0])
-		req.FrontendConnections        = count(counts[1])
-		req.BackendConnections         = count(counts[2])
-		req.ServerConnections          = count(counts[3])
-		req.Retries                    = count(counts[4])
+		req.ActiveConnections = count(counts[0])
+		req.FrontendConnections = count(counts[1])
+		req.BackendConnections = count(counts[2])
+		req.ServerConnections = count(counts[3])
+		req.Retries = count(counts[4])
 	}
 }
 
-func _string(s string) (*string) {
+func _string(s string) *string {
 	if s != "-" {
 		return &s
 	}
 	return nil
 }
 
-func _headers(s string) ([]string) {
+func _headers(s string) []string {
 	if headers := strings.Split(s, "|"); len(headers) > 0 {
 		return headers
 	}
@@ -186,7 +186,7 @@ func debug(matches []string) {
 	}
 }
 
-func Scan(line string, req *Request) (error) {
+func Scan(line string, req *Request) error {
 	var m []string
 
 	if m = Format.FindStringSubmatch(line); m == nil {
@@ -195,30 +195,30 @@ func Scan(line string, req *Request) (error) {
 
 	//debug(m)
 
-	req.Host                       = _string(m[2])
-	req.Process                    = _string(m[3])
-	req.Pid                        = _int32(m[4])
-	req.ClientIp                   = _ip(m[5])
-	req.ClientPort                 = _uint32(m[6])
+	req.Host = _string(m[2])
+	req.Process = _string(m[3])
+	req.Pid = _int32(m[4])
+	req.ClientIp = _ip(m[5])
+	req.ClientPort = _uint32(m[6])
 	scanTime(m[7], req)
-	req.Frontend                   = _string(m[8])
-	req.Backend                    = _string(m[9])
-	req.Server                     = _string(m[10])
+	req.Frontend = _string(m[8])
+	req.Backend = _string(m[9])
+	req.Server = _string(m[10])
 	scanTimings(m[11], req)
-	req.StatusCode                 = _int32(m[12])
-	req.BytesRead                  = _int32(m[13])
-	req.RequestCookie              = _string(m[14])
-	req.ResponseCookie             = _string(m[15])
+	req.StatusCode = _int32(m[12])
+	req.BytesRead = _int32(m[13])
+	req.RequestCookie = _string(m[14])
+	req.ResponseCookie = _string(m[15])
 	scanTermination(m[16], req)
 	scanConnections(m[17], req)
 	scanQueues(m[18], req)
 
-	req.RequestHeaders						= _headers(m[19])
-	req.ResponseHeaders						= _headers(m[20])
+	req.RequestHeaders = _headers(m[19])
+	req.ResponseHeaders = _headers(m[20])
 
-	req.HttpMethod								= _string(m[21])
-	req.HttpUri										= _string(m[22])
-	req.HttpVersion								= _string(m[23])
+	req.HttpMethod = _string(m[21])
+	req.HttpUri = _string(m[22])
+	req.HttpVersion = _string(m[23])
 
 	return nil
 }
